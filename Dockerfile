52ede24d31637e1f07fc962197eb468cb3cb5835
vaskinyy <yuri.vaskin@thetrainline.com>
2021-04-06 13:07:20 +0100

    Add a sample node app
    This app just returns hello world, it is tested, linted and dockerized.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:15
+
+WORKDIR /usr/src/app
+
+COPY app/package*.json ./
+
+RUN npm install
+COPY app/ .
+
+EXPOSE 8080
+CMD [ "node", "server.js" ]
---
+++ app/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
