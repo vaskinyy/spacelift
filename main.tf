3c5489f2ef6a360bb64ce5ed0fdbc6bc9206638b
vaskinyy <yuri.vaskin@thetrainline.com>
2021-04-08 15:24:24 +0100

    Clean up the workflows

--- main.tf
+++ main.tf
@@ -40,33 +40,6 @@ resource "aws_ecs_cluster" "spacelift_cluster" {
   name = "spacelift-cluster" # Naming the cluster
 }
 
-
-//resource "aws_ecs_task_definition" "spacelift_task" {
-//  family                   = "spacelift-task" # Naming our first task
-//  container_definitions    = <<DEFINITION
-//  [
-//    {
-//      "name": "spacelift-task",
-//      "image": "${aws_ecr_repository.spacelift.repository_url}:latest",
-//      "essential": true,
-//      "portMappings": [
-//        {
-//          "containerPort": 8080,
-//          "hostPort": 8080
-//        }
-//      ],
-//      "memory": 512,
-//      "cpu": 256
-//    }
-//  ]
-//  DEFINITION
-//  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-//  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-//  memory                   = 512         # Specifying the memory our container requires
-//  cpu                      = 256         # Specifying the CPU our container requires
-//  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
-//}
-
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
@@ -134,27 +107,6 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
   }
 }
-
-//resource "aws_ecs_service" "spacelift_service" {
-//  name            = "spacelift-service"                             # Naming our first service
-//  cluster         = "${aws_ecs_cluster.spacelift_cluster.id}"             # Referencing our created Cluster
-//  task_definition = "${aws_ecs_task_definition.spacelift_task.arn}" # Referencing the task our service will spin up
-//  launch_type     = "FARGATE"
-//  desired_count   = 3 # Setting the number of containers to 3
-//
-//  load_balancer {
-//    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-//    container_name   = "${aws_ecs_task_definition.spacelift_task.family}"
-//    container_port   = 8080 # Specifying the container port
-//  }
-//
-//  network_configuration {
-//    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-//    assign_public_ip = true                                                # Providing our containers with public IPs
-//    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
-//  }
-//}
-
 
 resource "aws_security_group" "service_security_group" {
   ingress {
