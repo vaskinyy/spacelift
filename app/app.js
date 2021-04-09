551cee720ac5a7604e9499b57c3672f0b54ff863
vaskinyy <yuri.vaskin@thetrainline.com>
2021-04-09 09:38:04 +0100

    Change the message

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  res.status(200).send('Hello Vin!');
 });
 
 module.exports = app;
