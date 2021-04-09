57dbc761ee980c43a90bef42e41b0474e7826ac8
vaskinyy <yuri.vaskin@thetrainline.com>
2021-04-09 11:53:31 +0100

    Change to hello

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Vin!');
+  res.status(200).send('Hello World!');
 });
 
 module.exports = app;
